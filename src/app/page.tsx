deeebc239177d9cce8b682eb6a34065e94668be8
Nina S <n.m.sokolnikova06@gmail.com>
2026-01-24 21:30:15 -0800

    upload pizza drawings and landing images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
 "use client";
 import Image from "next/image";
-
-
 const Home = () => {
   return (
-    <Image
-        src="/SOSPIZZA.png"
-        alt="SOS PIZZA"
-        width={360}
-        height={360}
-      />
-  );
+    <div className="min-h-screen bg-creme flex flex-col w-full relative items-center">
+      <div className="w-full h-48 overflow-hidden">
+        <Image src="/Checkerboard.svg" alt="SOS PIZZA" width={5000} height={500} />
+      </div>
+      <div className="flex absolute flex-col w-1/2 top-32">
+        <Image src="/SOSPIZZA.png" alt="SOS PIZZA" width={818} height={199} />
+      <div className="flex absolute top-1/3">
+        <Image src="/SOSpizza.svg" alt="SOS PIZZA" width={2148} height={370} />
+      </div>
+      <div className="flex relative">
+        <Image src="/localtext.png" alt="locally sourced and tailored to you" width={700} height={700} />
+      </div>
+      <div className="flex flex-row absolute font-ultra text-5xl space-x-40 top-1/2">
+        <div>EST.</div>
+        <div>2026</div>
+      </div>
+      </div>
+    </div>
+    );
 };
 export default Home;
