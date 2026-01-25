f9434ca61cb76ecc7d24b37f361863f73fcd5293
LawMot <molawr12@gmail.com>
2026-01-24 17:41:23 -0800

    created navbar

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+  return (
+    <div className="from-hcg-black to-hcg-gold justify-between bg-linear-to-r px-6 py-4 md:flex md:items-end"></div>
+  );
+};
+
+export default Footer;
