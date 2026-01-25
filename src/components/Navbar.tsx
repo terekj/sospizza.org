5fe1478c72aae76a0d2e615800658c89cf2a66fb
LawMot <molawr12@gmail.com>
2026-01-24 17:52:57 -0800

    fix href and font

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <div className="flex bg-red h-24 items-center justify-between p-4">
-      <Link href="/">
+      <Link href="./">
         <div className="flex w-full">
           <Image
             src="/SOSpizzaMini1.svg"
@@ -15,8 +15,8 @@ const Navbar = () => {
         </div>
       </Link>
 
-      <Link href="/app/Order">
-        <div className="flex items-center p-2 px-4 text-xl text-white border-3 rounded-3xl border-red-950 whitespace-nowrap bg-gray-50/25 drop-shadow-lg">
+      <Link href="../Order">
+        <div className="flex items-center p-2 px-4 text-xl text-white border-3 rounded-3xl border-red-950 whitespace-nowrap bg-gray-50/25 drop-shadow-lg font-ultra">
           Order Now
         </div>
       </Link>
